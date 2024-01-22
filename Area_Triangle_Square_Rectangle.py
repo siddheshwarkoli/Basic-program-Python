dd564e5723af49ff4872a1c5624c8a561b23c32f
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-22 12:05:32 +0530

    Initial Commit

---
+++ Area_Triangle_Square_Rectangle.py
@@ -0,0 +1,20 @@
+# Python program to calculate area of Triangle, Square & Rectangle
+
+print("----Input Values For Triangle----")
+h=float(input("Enter Height of triangle= "))
+b=float(input("Enter Base of triangle= "))
+area=0.5*h*b
+print('Area of Triangle is=  ', area)
+
+
+print("----Input Values For Square----")
+s=float(input("Enter Side of Square= "))
+area=s*s
+print('Area of Square is= ', area)
+
+
+print("----Input Values For Rectangle----")
+l=float(input("Enter Length of Rectangle= "))
+w=float(input("Enter Width of Rectangle= "))
+area=l*w
+print('Area of Triangle is=  ', area)
