e83660f4e01c250c1b10ab15ae66c2a59c05fa29
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-22 12:06:21 +0530

    Initial Commit

---
+++ celsius_to_fehrenheit.py
@@ -0,0 +1,6 @@
+# Python program to convert celsius to fehrenheit.
+
+
+celsius=int(input("Enter a number="))
+fehrenheit=(celsius*1.8)+32
+print(celsius,"Degree celsius is equal to", fehrenheit,"degree Fehrenheit.")
