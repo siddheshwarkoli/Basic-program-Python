7c6a81d26ea930a157cd8f2555dcff4e145e55b6
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-22 12:07:36 +0530

    Initial Commit

---
+++ count_of_digit.py
@@ -0,0 +1,6 @@
+# Wap to count the number of digits in an integer
+
+num=int(input("Enter Any Number: "))
+string=str(num)
+length=len(string)
+print("Total number of Digitd in given number is: ",length)
