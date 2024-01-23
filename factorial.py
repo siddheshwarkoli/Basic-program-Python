fa068be634a13009ef8b682599f11461a168f64d
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-23 17:06:23 +0530

    Initial Commit

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# WAP to Find the Factorial of a Number
+
+num = int(input("Enter a number: "))
+fact = 1
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1.")
+else:
+    for i in range(1, num + 1):
+        fact = fact * i
+    print("The factorial of ", num, " is " ,fact)
