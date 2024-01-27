b71da5b979f0d0c6123755c93198c34901fba4ba
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-27 21:46:09 +0530

    Initial Commit

---
+++ Palindrome_number.py
@@ -0,0 +1,14 @@
+# Python code to check the given number is palindrome or not.
+
+
+num=int(input("Enter a number="))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is a palindrome")
+else:
+    print("The number is not a palindrome")
