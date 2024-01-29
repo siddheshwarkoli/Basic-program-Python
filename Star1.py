4cd649eafd18d9304afbc21258f7f60dfb3660ba
Siddheshwar Koli <144483447+siddheshwarkoli@users.noreply.github.com>
2024-01-30 01:21:29 +0530

    Initial Commit

---
+++ Star1.py
@@ -0,0 +1,21 @@
+#------------Enter Your Second Name------------
+name=input("Enter your name: ")
+x=0
+for i in name:
+    x=x+1
+    print(name[0:x])
+for i in name:
+    x=x-1
+    print(name[0:x])
+    
+    
+    
+#------------Enter Your Second Name------------
+name=input("Enter your name: ")
+x=0
+for i in name:
+    x=x+1
+    print(name[0:x])
+for i in name:
+    x=x-1
+    print(name[0:x])
